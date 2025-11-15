82c14f39e983165897399864189fcf1d134c25e9
mmmuning <217378167+mmmuning@users.noreply.github.com>
2025-11-15 12:13:02 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function Book(title, author, pages, status) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
